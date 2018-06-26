eb0244377554a3a84224b57cc6b72e6067fff0ea
coniekp <coniekp@icloud.com>
2018-06-26 14:17:07 -0700

    Adjust videoPlayerView to handle data

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -1,11 +1,14 @@
 var VideoPlayerView = Backbone.View.extend({
+  
   initialize: function (){
     this.listenTo(this.collection, 'select', this.selectVideo);
+    this.model  = this.collection.at(0);
     this.render();
   },
   
-  selectVideo: function (selection) {
-    this.model = selection;
+  
+  selectVideo: function (selected) {
+    this.model = selected || this.model;
     this.render();
   },
 
@@ -13,8 +16,19 @@ var VideoPlayerView = Backbone.View.extend({
     if(!this.model){
       this.$el.html('<div class="loading">Please wait...</div>');
     } else {
+      var url = 'https://www.youtube.com/embed/' + this.model.get('id');
+      var info = this.model.get('snippet');
+      
       this.$el.html(this.template());
+      
+      //Changing template's default values
+      this.$el.find('iframe').attr('src', url);
+      this.$el.find('h3').html(info.title);
+      this.$el.find('.video-player-details div').html(info.description);
+
+      //{snippet: this.model.attributes.snippet}
     }
+    
     return this;
   },
 
